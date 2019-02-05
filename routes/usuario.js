b2a8ef9a1d0e341737f698e78788349d9ce07af3
lionel <lioneltolosaarmas@gmail.com>
2019-02-05 11:33:29 +0100

    Create PUT AND DELETE USUARIOS

--- routes/usuario.js
+++ routes/usuario.js
@@ -48,7 +48,64 @@ app.get('/', (req, res, next) => {
 
 
 
-    // Post
+
+    // PUt Actualizar usuario
+    app.put('/:id', (req, res) => {
+
+        var id = req.params.id
+        var body = req.body;
+
+        Usuario.findById( id, (err, usuario) => {
+
+            if (err) {
+                return res.status(500).json({  // 500 Internal Server Error
+                    ok: false,
+                    mensaje: 'Error al buscar usuario',
+                    errors: err
+                });
+            }
+
+            if( !usuario) {
+                return res.status(400).json ({
+                    ok: false,
+                    mensaje: 'El usuario con el' + id + 'no existe',
+                    errors: {mensaje: 'No existe un usuraio con ese ID'}
+                })
+            }
+
+            usuario.name = body.name;
+            usuario.email = body.email;
+            usuario.role = body.role;
+
+            usuario.save((err, usuarioGuardado) => {
+    
+                if (err) {
+                    return res.status(400).json({
+                        ok: false,
+                        mensaje: 'Error al actualizar usuario',
+                        errors: err
+                    });
+                }
+
+                usuarioGuardado.password = ':)';
+        
+                res.status(200).json({ 
+                    ok: true,
+                    usuario: usuarioGuardado,
+                });
+            });
+        });
+    });
+
+
+
+
+
+
+
+
+
+    // Post   Crear nuevo usuario
 
     app.post('/', (req, res) => {
 
@@ -78,10 +135,44 @@ app.get('/', (req, res, next) => {
                 usuario: usuarioGuardado,
                 // usuariotoken: req.usuario
             });
-    
-    
         });
-    
     });
+
+
+
+
+//   Borrar un usuario por el id
+
+// app.delete('/:id', mdAutenticacion.verificaToken, (req, res) => {
+app.delete('/:id', (req, res) => {
+
+
+    var id = req.params.id;
+
+    Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
+
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error borrar usuario',
+                errors: err
+            });
+        }
+
+        if (!usuarioBorrado) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'No existe un usuario con ese id',
+                errors: { message: 'No existe un usuario con ese id' }
+            });
+        }
+
+        res.status(200).json({
+            ok: true,
+            usuario: usuarioBorrado
+        });
+
+    });
+});
 
 module.exports = app;
